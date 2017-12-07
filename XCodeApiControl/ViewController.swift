34ea17d0db68b2b26661054671fde478704c7f59
Shofikul <shofikul19922@gmail.com>
2017-12-07 02:02:59 -0800

    Initial Commit

---
+++ XCodeApiControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCodeApiControl
+//
+//  Created by Admin on 07/12/2017.
+//  Copyright © 2017 houseRent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
